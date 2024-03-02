429d5179e2d6d218db6f0dbb057d9369e584c11e
tamimhasan19702 <tamimhasancu@gmail.com>
2024-03-03 00:39:00 +0600

    worked with settings api to integrate it into plugin frontend

--- word-count.php
+++ word-count.php
@@ -16,17 +16,44 @@ class WordCountPluginAndTime
     {
 
         add_action('admin_menu', [$this, 'adminMenu']);
+        add_action('admin_init', [$this, 'settings']);
+    }
+
+    function settings()
+    {
+        add_settings_section('wcp_first_section', null, null, 'word-count');
+        add_settings_field('wcp_location', 'Display Location', [$this, 'locationHtml'], 'word-count', 'wcp_first_section', );
+        register_setting('word-count-settings-group', "wcp_location", ['sanitize_callback' => 'sanitize_text_field', 'default' => '']);
     }
     function adminMenu()
     {
         add_submenu_page('options-general.php', 'Word Count Settings', 'Word Count', 'manage_options', 'word-count', [$this, 'mainPage']);
     }
 
+    function locationHtml()
+    {
+        ?>
+        <select name="wcp_location">
+            <option value="0">Begining of the Page</option>
+            <option value="1">Ending of the Page</option>
+        </select>
+        <?php
+    }
     function mainPage()
     {
         ?>
-<h1>Hello world</h1>
-<?php
+        <div class="wrap">
+
+            <h1>Hello world</h1>
+            <form action="options.php" method="post">
+                <?php
+                settings_fields("word-count-settings-group");
+                do_settings_sections("word-count");
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
 
     }
 
