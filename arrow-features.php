8c0d9535d2fda98df5143a7717bc33f24563cb06
tamimhasan19702 <tamimhasancu@gmail.com>
2024-07-13 02:57:11 +0600

    delete 11 files

---
+++ arrow-features.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+ * Plugin Name:       Arrow Features
+ * Description:       adds gutenburg blocks
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Tareq Monower
+ * Author URI:        https://tamimhasan19702.github.io/portfolio/#about
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       arrow-features
+ * Domain Path:       /languages
+ */
+
+
+?>
